bcfda159edf41b782b1101f050ddf08210ce2008
Xi Chen <xichen827@gmail.com>
2014-11-26 09:41:57 -0800

    ice-cream show page with composite view of review items

--- app/assets/javascripts/views/ice_creams/ice_cream_show.js
+++ app/assets/javascripts/views/ice_creams/ice_cream_show.js
@@ -1,15 +1,29 @@
-XiFinalProject.Views.IceCreamShow = Backbone.View.extend({
+XiFinalProject.Views.IceCreamShow = Backbone.CompositeView.extend({
 
   template: JST['ice_creams/show'],
 
   initialize: function(){
+  	this.reviews = this.model.reviews();
   	this.listenTo(this.model, "sync", this.render);
+  	this.listenTo(this.reviews, "sync", this.render);
+  	this.listenTo(this.reviews, "add", this.addReview);
+  	this.reviews.each(function(review){
+  		this.addReview(review)
+  	}.bind(this))
+  },
+
+  addReview: function(review){
+  	var reviewItem = new XiFinalProject.Views.ReviewIndexItem({model: review})
+  	this.addSubview(".reviews-list", reviewItem)
   },
 
   render: function(){
   	var content = this.template({ice_cream: this.model});
   	this.$el.html(content);
+  	this.attachSubviews()
   	return this;
   }
 
 });
+
+
